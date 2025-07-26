194637ac0efca1c39ea9573a6d49de82a9f6e681
jmc0504 <92536620+JeonMinCheol@users.noreply.github.com>
2025-07-27 00:54:12 +0900

    Update mysql_table_create.sql

--- mysql_table_create.sql
+++ mysql_table_create.sql
@@ -1,3 +1,22 @@
+create database justice_ben;
+use justice_ben;
+
+-- delete from justice_ben.user;
+-- delete from justice_ben.case_decision;
+-- delete from justice_ben.case_info;
+-- delete from justice_ben.lawyer_info;
+-- delete from justice_ben.defendant_lawyer;
+-- delete from justice_ben.plaintiff_lawyer;
+
+-- DROP TABLE user;
+-- DROP TABLE defendant_lawyer;
+-- DROP TABLE plaintiff_lawyer;
+-- DROP TABLE case_decision;
+-- DROP TABLE case_info;
+-- DROP TABLE lawyer_info;
+
+-- select * from case_info;
+
 CREATE TABLE case_decision (
 _id int DEFAULT NULL,
 CASE_ID int DEFAULT NULL,
@@ -6,7 +25,7 @@ PARAGRAPH text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE case_info (
-CASE_ID int NOT NULL AUTO_INCREMENT,
+CASE_ID int NOT NULL,
 CASE_NAME varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
 CASE_KIND varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
 INCIDENT_REASON text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
@@ -23,20 +42,20 @@ SUMMARY text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
 RESULT varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
 PRIMARY KEY (CASE_ID),
 KEY idx_decision_date (DECISION_DATE)
-) ENGINE=InnoDB AUTO_INCREMENT=108004 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `lawyer_info` (
-  `_id` int NOT NULL AUTO_INCREMENT,
+  `_id` int NOT NULL,
   `LAWYER_NAME` varchar(256) NOT NULL,
   `LAWFIRM` varchar(1024) DEFAULT NULL,
   `COUNT` int DEFAULT NULL,
   `WIN` int DEFAULT NULL,
   `LOSE` int DEFAULT NULL,
   PRIMARY KEY (`_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=226802 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `defendant_lawyer` (
-  `_id` int NOT NULL AUTO_INCREMENT,
+  `_id` int NOT NULL,
   `CASE_ID` int NOT NULL,
   `LAWYER_NO` int NOT NULL,
   `LAWYER_NAME` varchar(1024) NOT NULL,
@@ -47,10 +66,10 @@ CREATE TABLE `defendant_lawyer` (
   KEY `LAWYER_NO` (`LAWYER_NO`),
   CONSTRAINT `defendant_lawyer_ibfk_1` FOREIGN KEY (`CASE_ID`) REFERENCES `case_info` (`CASE_ID`) ON UPDATE CASCADE,
   CONSTRAINT `defendant_lawyer_ibfk_2` FOREIGN KEY (`LAWYER_NO`) REFERENCES `lawyer_info` (`_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=264948 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `plaintiff_lawyer` (
-  `_id` int NOT NULL AUTO_INCREMENT,
+  `_id` int NOT NULL,
   `CASE_ID` int NOT NULL,
   `LAWYER_NO` int NOT NULL,
   `LAWYER_NAME` varchar(1024) NOT NULL,
@@ -61,7 +80,7 @@ CREATE TABLE `plaintiff_lawyer` (
   KEY `LAWYER_NO` (`LAWYER_NO`),
   CONSTRAINT `plaintiff_lawyer_ibfk_1` FOREIGN KEY (`CASE_ID`) REFERENCES `case_info` (`CASE_ID`) ON UPDATE CASCADE,
   CONSTRAINT `plaintiff_lawyer_ibfk_2` FOREIGN KEY (`LAWYER_NO`) REFERENCES `lawyer_info` (`_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=97988 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `user` (
   `_id` int NOT NULL AUTO_INCREMENT,
@@ -69,4 +88,18 @@ CREATE TABLE `user` (
   `password` varchar(256) NOT NULL,
   `role` varchar(256) NOT NULL,
   PRIMARY KEY (`_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 데이터 임포트 완료 후 실행할 것
+-- ALTER TABLE defendant_lawyer DROP FOREIGN KEY defendant_lawyer_ibfk_2;
+-- ALTER TABLE plaintiff_lawyer DROP FOREIGN KEY plaintiff_lawyer_ibfk_2;
+-- ALTER TABLE lawyer_info MODIFY COLUMN _id INT NOT NULL AUTO_INCREMENT;
+-- ALTER TABLE defendant_lawyer
+-- ADD CONSTRAINT defendant_lawyer_ibfk_2
+-- FOREIGN KEY (LAWYER_NO) REFERENCES lawyer_info(_id) ON UPDATE CASCADE;
+-- ALTER TABLE plaintiff_lawyer
+-- ADD CONSTRAINT plaintiff_lawyer_ibfk_2
+-- FOREIGN KEY (LAWYER_NO) REFERENCES lawyer_info(_id) ON UPDATE CASCADE;
+-- ALTER TABLE defendant_lawyer MODIFY COLUMN _id INT NOT NULL AUTO_INCREMENT;
+-- ALTER TABLE plaintiff_lawyer MODIFY COLUMN _id INT NOT NULL AUTO_INCREMENT;
+-- ALTER TABLE case_decision MODIFY COLUMN _id INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (_id);
